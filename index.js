50aab28062a064550d164fb200ac78c69b947f8f
Ben Armstrong <benarmstrong@live.com>
2023-02-16 22:28:55 +1030

    All file structures created and draft README

---
+++ index.js
@@ -0,0 +1,24 @@
+//-------------------//**BEN to amend below**/
+
+const teamProfileGenerator = require("./lib/Game");
+
+// Initialize a new Team Profile Generator object
+const teamProfileGenerator = new teamProfileGenerator();
+
+// Start playing
+teamProfileGenerator.play();
+
+
+
+
+
+
+
+// // taken from mini project initialiser
+// const Game = require("./lib/Game");
+
+// // Initialize a new Game object
+// const game = new Game();
+
+// // Start playing
+// game.play();
